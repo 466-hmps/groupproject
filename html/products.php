6b57ae63c60901eb4026b0d682c3f24a3ecc3c32
Marrugaloo <z1689918@students.niu.edu>
2022-05-04 01:34:00 -0500

    Complete Product page

--- html/products.php
+++ html/products.php
@@ -2,73 +2,36 @@
 $title = "Products";
 include '../partials/header.php';
 include '../partials/nav.php'; 
-?>
-    <h1>Products</h1>
+include '../Model/dbstarter.php';
+include '../Model/dbhandler.php';
+$products = AllProducts();
+$count = count($products);
+echo "    <h1>Products</h1>\n";
+for($i=0; $i<$count; $i=$i+2) {
+    echo "    <div class=\"row\">\n";
+    for($j=0; $j<=1; $j++)
+    {
+        echo <<<HTML
+            <div class="column">
+                <h2>{$products[$i+$j]['ProdType']} \${$products[$i+$j]['Price']} per {$products[$i+$j]['Weight']} lb(s)</h2>
+                <img width="150" height="150" src="img/{$products[$i+$j]['ProdType']}.jpg" alt="{$products[$i+$j]['ProdType']} Products."><br>
+                <p>
+                    {$products[$i+$j]['Description']}
+                </p>
+                <form>
+                    <input type="hidden" name="prodid" value="{$products[$i+$j]['ProdID']}">
+                    <label for="amount">QTY</label>
+                    <input type='number' name='amount' value='QTY of item' />
+                    <input type='submit' name='add' value='Add to cart' />
+                </form>
+            </div>
+        HTML;
+    
+        if(!isset($products[$i+1])) break;
+    }
+    echo "    </div>\n";
+}
 
-    <h2>Beef</h2>
-    <div class="grid-container">
-      <div class="grid-item">
-      <img width="150" height="150" src="img/beef.jpg" alt="Beef Products.">
-      </div>
-      <div class="grid-item"><p>Ground Beef. 85% Fat and 15% Lean.</p><p>Great for burgers or making burgers or meatballs.</p></div>
-      <div class="grid-item">
-            <label for="amount">QTY</label>
-            <input type='number' name='amount' value='QTY of item' />
-            <input type='submit' name='add' value='Add to cart' />
-      </div>
-    </div>
-
-    <h2>Pork</h2>
-    <div class="grid-container">
-      <div class="grid-item">
-      <img width="150" height="150" src="img/pork.jpg" alt="God's gift to burgers.">
-      </div>
-      <div class="grid-item"><p>Breaded Pork Chops</p><p>Great for a wholesome family dinners or gatherings.</p></div>
-      <div class="grid-item">
-            <label for="amount">QTY</label>
-            <input type='number' name='amount' value='QTY of item' />
-            <input type='submit' name='add' value='Add to cart' />
-      </div>
-    </div>
-
-    <h2>Chicken</h2>
-    <div class="grid-container">
-      <div class="grid-item">
-      <img width="150" height="150" src="img/chicken.jpg" alt="BAWK BAWK">
-      </div>
-      <div class="grid-item"><p>Boneless/Skinless Chicken Breast</p><p>Wonderfull for all purpose meals. Can be used for your chicken tacos, fettucini alfredo, or even your protein sourcce for macro conscious lifters.</p></div>
-      <div class="grid-item">
-            <label for="amount">QTY</label>
-            <input type='number' name='amount' value='QTY of item' />
-            <input type='submit' name='add' value='Add to cart' />
-      </div>
-    </div>
-
-    <h2>Turkey</h2>
-    <div class="grid-container">
-      <div class="grid-item">
-      <img width="150" height="150" src="img/turkey.jpg" alt="Gobble Gobble">
-      </div>
-      <div class="grid-item"><p>Ground Turkey</p><p>Great choice for a more healthy alternative for meatballs or burgers.</p></div>
-      <div class="grid-item">
-            <label for="amount">QTY</label>
-            <input type='number' name='amount' value='QTY of item' />
-            <input type='submit' name='add' value='Add to cart' />
-      </div>
-    </div>
-
-    <h2>Fish</h2>
-    <div class="grid-container">
-      <div class="grid-item">
-      <img width="150" height="150" src="img/fish.jpg" alt="Fish are friends, not food">
-      </div>
-      <div class="grid-item"><p>Salmon Fillets. Wild Alaskan Salmon.</p><p>Healthy meat alternative for a reduction of red meat products.</p><p>Great for grilling or pan searing on a bed of rice.</p></div>
-      <div class="grid-item">
-            <label for="amount">QTY</label>
-            <input type='number' name='amount' value='QTY of item' />
-            <input type='submit' name='add' value='Add to cart' />
-      </div>
-    </div>
-<?php include '../partials/footer.php'; ?>
+ include '../partials/footer.php'; ?>
 
 
--- sql/sample-data.sql
+++ sql/sample-data.sql
@@ -10,12 +10,12 @@ INSERT INTO CUSTOMER(CustID, CardNum, CardExp, CVV) VALUES
   ('1', '0000000000000001',  '2023-01-01', '123'),
   ('3', '1234567890987654', '1776-07-01', '890');
 
-INSERT INTO PRODUCT(ProdID, ProdType, Title, Weight, Price, QTY) VALUES
-  ('1', 'Beef', 'Ground Beef', '1.00', '9.99', '20'),
-  ('2', 'Pork', 'Pork Chop', '3.00', '6.99', '10'),
-  ('3', 'Chicken', 'Chicken Breast', '2.00', '8.99', '6'),
-  ('4', 'Turkey', 'Ground Turkey', '0.50', '5.99', '4'),
-  ('5', 'Salmon', 'Salmon Filet', '5.00', '19.99', '17');
+INSERT INTO PRODUCT(ProdID, ProdType, Title, Weight, Price, QTY, Description) VALUES
+  ('1', 'Beef', 'Ground Beef', '1.00', '9.99', '20', "Ground Beef. 85% Fat and 15% Lean.<br>Great for burgers or making burgers or meatballs."),
+  ('2', 'Pork', 'Pork Chop', '3.00', '6.99', '10', "Breaded Pork Chops<br>Great for a wholesome family dinners or gatherings."),
+  ('3', 'Chicken', 'Chicken Breast', '2.00', '8.99', '6', "Boneless/Skinless Chicken Breast. Wonderfull for all purpose meals.<br> Can be used for your chicken tacos, fettucini alfredo, or even your protein sourcce for macro conscious lifters."),
+  ('4', 'Turkey', 'Ground Turkey', '0.50', '5.99', '4', "Fresh Ground turkey by the pound.<br>Great choice for a more healthy alternative for meatballs or burgers."),
+  ('5', 'Salmon', 'Salmon Filet', '5.00', '19.99', '17', "Salmon Fillets. Wild Alaskan Salmon. Healthy meat alternative for a reduction<br> of red meat products. Great for grilling or pan searing on a bed of rice.");
 
 INSERT INTO ORDERS(OrderID, CustID, Status, Total) VALUES
   ('1', '3', 'Shipped', '127.87'),
