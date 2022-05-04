ebf55d4880869c41d06c6130d4926769b40aca79
Marrugaloo <z1689918@students.niu.edu>
2022-05-04 00:16:23 -0500

    Add line to footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,4 @@
+<hr>
 <div class="footer">
         <span>Painstackingly created by Group 17 of Lehuta's CSCI 466 Class of Spring 2022. Please give A. Much Thanks :) &copy; HMPS</span>    
     </div>
