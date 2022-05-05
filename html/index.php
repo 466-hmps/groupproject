9fdaf4208b15de2f1a6ef3c598b0adedd6cd1adf
khiltenb <75854549+khiltenb@users.noreply.github.com>
2022-05-05 18:58:05 -0500

    Once more, array index search.

--- html/index.php
+++ html/index.php
@@ -5,6 +5,11 @@ include '../partials/header.php';
 include '../partials/nav.php'; 
 ?>
     <p>As a humble group of butchers, we seek to deliver only the finest of meats to satisfy true carnal appetite. </p>
-    <?php $wah = getUserType($_SESSION['userid']);
-    echo "UserType = " . $wah['Email'];?>
+    <?php
+    if(isset($_SESSION['userid']))
+        {
+            $wah = getUserType($_SESSION['userid']);
+            echo "UserType = " . $wah['Email'];
+        }
+    ?>
 <?php include '../partials/footer.php'; ?>
