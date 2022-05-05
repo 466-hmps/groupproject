bda31928e114b33606f6402bbdc717c413c5f166
Marrugaloo <z1689918@students.niu.edu>
2022-05-05 04:46:28 -0500

    Add draw_cart function

--- lib/library.php
+++ lib/library.php
@@ -20,4 +20,25 @@ function dump_table(&$pdo, $table)
 {
     draw_table($pdo->query("SELECT * FROM $table;")->fetchAll(PDO::FETCH_ASSOC));
 }
+function draw_cart($rows)
+{
+    echo "    <table class='cart'\n";
+    echo "        <tr>\n";
+    echo "            <th width=100>Picture</th>\n";
+    echo "            <th>Title</th>\n";
+    echo "            <th>Weight</th>\n";
+    echo "            <th>Price</th>\n";
+    echo "            <th>Amt</th>\n"; 
+    echo "        </tr>\n";
+    foreach ($rows as $row) {
+        echo "        <tr>\n";
+        echo "            <td width=100><img height=100 width=100 src='img/{$row['ProdType']}.jpg'></td>\n";
+        echo "            <td>{$row['Title']}</td>\n";
+        echo "            <td>{$row['Weight']}</td>\n";
+        echo "            <td>{$row['Price']}</td>\n";
+        echo "            <td>{$row['Amt']}</td>\n";
+        echo "        </tr>\n";
+    }
+    echo "    </table>\n";
+}
 ?>
