2e345a055dd29719cf0b62e7778542ddd843a813
khiltenb <75854549+khiltenb@users.noreply.github.com>
2022-05-05 16:25:23 -0500

    messed something up

--- html/index.php
+++ html/index.php
@@ -5,5 +5,5 @@ include '../partials/header.php';
 include '../partials/nav.php'; 
 ?>
     <p>As a humble group of butchers, we seek to deliver only the finest of meats to satisfy true carnal appetite. </p>
-    <?php echo "UserType = " . getUserType($_POST['uemail']);?>
+    <?php echo "UserType = " . getUserType($_SESSION['userid']);?>
 <?php include '../partials/footer.php'; ?>
