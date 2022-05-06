cc065a289ad2be7030cb632391ddce7c7f133305
khiltenb <75854549+khiltenb@users.noreply.github.com>
2022-05-05 20:00:08 -0500

    fixed something

--- html/index.php
+++ html/index.php
@@ -5,12 +5,4 @@ include '../partials/header.php';
 include '../partials/nav.php'; 
 ?>
     <p>As a humble group of butchers, we seek to deliver only the finest of meats to satisfy true carnal appetite. </p>
-    <?php
-    if(isset($_SESSION['userid']))
-        {
-            $wah = getUserType($_SESSION['userid']);
-            echo "UserType = " . $wah;
-            echo $_SESSION['Checkmate'];
-        }
-    ?>
 <?php include '../partials/footer.php'; ?>
