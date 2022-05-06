9ccda950595e699a57127101affd912513e84675
Marrugaloo <z1689918@students.niu.edu>
2022-05-05 23:14:27 -0500

    Fix issue with clearing order.

--- html/checkout.php
+++ html/checkout.php
@@ -4,24 +4,31 @@ $title = "Checkout";
 include '../partials/header.php';
 include '../partials/nav.php'; 
 include '../lib/library.php';
-include '../partials/printglobals.php';
 if(isset($_SESSION['userid'])) {
     $total = checkout_cart(GetCart($_SESSION['userid']));
-    echo <<<HTML
-        <form method="POST">
-            <span>
-                <label for="checkout">
-                <button type="submit" name="checkout" id="checkout" value="checkout">Checkout</button>
-            </span>
-        </form>
-    HTML;
+    if ($total == 0) {
+        echo "  <h2>Your Cart Is Empty</h2>";
+    }
+    else
+    {
+        echo <<<HTML
+            <form method="POST">
+                <span>
+                    <label for="checkout">
+                    <button type="submit" name="checkout" id="checkout" value="checkout">Checkout</button>
+                </span>
+            </form>
+        HTML;
+    }
 }
 else
     echo "  <p>Your cart is empty. Please login to checkout.</p>";
 if(isset($_POST['checkout']))
 {
     CreateOrder($_SESSION['userid'], $total);
-    ClearCart($_SESSION['custid']);
+    ClearCart($_SESSION['userid']);
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/');
+    exit;
 }
 ?>
 
