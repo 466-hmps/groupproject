2aa1a74b30e8c2aa68506396246031167d350183
khiltenb <75854549+khiltenb@users.noreply.github.com>
2022-05-05 22:09:32 -0500

    forgot to add a customer ID

--- html/cart.php
+++ html/cart.php
@@ -7,7 +7,7 @@ include '../lib/library.php';
 echo "  <h3 class='cartheader'>This page shows your current cart</h3>";
 if(isset($_SESSION['userid'])){
     if(isset($_POST['RemoveItem'])) {
-        RMFromCart($CustID, $_POST['RemoveItem']);
+        RMFromCart($_SESSION['userid'], $_POST['RemoveItem']);
     }
     draw_cart(GetCart($_SESSION['userid']));
     $rows = GetCart($_SESSION['userid']);
